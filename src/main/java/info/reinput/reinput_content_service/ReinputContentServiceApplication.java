8866abdf829148f82288167c16c82eae19b3a857
MinhoJJang <ahchjang@naver.com>
2025-01-14 17:03:14 +0900

    Initial commit: Set up Spring Boot project with Gradle, including application configuration, build scripts, and basic structure. Added .gitignore and .gitattributes for environment-specific settings.

---
+++ src/main/java/info/reinput/reinput_content_service/ReinputContentServiceApplication.java
@@ -0,0 +1,15 @@
+package info.reinput.reinput_content_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ReinputContentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReinputContentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/reinput/reinput_content_service/ReinputContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package info.reinput.reinput_content_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReinputContentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
