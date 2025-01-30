b64168364109840e2b2e9fcc7c98bb30afef013e
Jhsysng <ysng1118@naver.com>
2025-01-30 16:08:25 +0900

    RE-179 chore: feign 활성화 -enable jpa auditing -mysql 의존성 추가

--- src/main/java/info/reinput/reinput_content_service/ReinputContentServiceApplication.java
+++ src/main/java/info/reinput/reinput_content_service/ReinputContentServiceApplication.java
@@ -3,9 +3,13 @@ package info.reinput.reinput_content_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing
+@EnableFeignClients
 public class ReinputContentServiceApplication {
 
 	public static void main(String[] args) {
